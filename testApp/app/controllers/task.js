b94283db2d24da255b75facd765f4d3078b3ec65
Eevert Saukkokoski <eevert.saukkokoski@appgyver.com>
2014-04-09 15:02:17 +0300

    deprecate TaskRestangular and use TaskResource instead

--- testApp/app/controllers/task.js
+++ testApp/app/controllers/task.js
@@ -3,7 +3,7 @@ var taskApp = angular.module('taskApp', ['TaskModel', 'hmTouchevents']);
 
 // Index: http://localhost/views/task/index.html
 
-taskApp.controller('IndexCtrl', function ($scope, TaskRestangular) {
+taskApp.controller('IndexCtrl', function ($scope, TaskResource) {
 
   // Helper function for opening new webviews
   $scope.open = function(id) {
@@ -12,7 +12,7 @@ taskApp.controller('IndexCtrl', function ($scope, TaskRestangular) {
   };
 
   // Fetch all objects from the local JSON (see app/models/task.js)
-  $scope.tasks = TaskRestangular.all('task').getList();
+  $scope.tasks = TaskResource.findAll();
 
   // -- Native navigation
   steroids.view.navigationBar.show("Task index");
@@ -22,10 +22,10 @@ taskApp.controller('IndexCtrl', function ($scope, TaskRestangular) {
 
 // Show: http://localhost/views/task/show.html?id=<id>
 
-taskApp.controller('ShowCtrl', function ($scope, $filter, TaskRestangular) {
+taskApp.controller('ShowCtrl', function ($scope, $filter, TaskResource) {
 
   // Fetch all objects from the local JSON (see app/models/task.js)
-  TaskRestangular.all('task').getList().then( function(tasks) {
+  TaskResource.findAll().then( function(tasks) {
     // Then select the one based on the view's id query parameter
     $scope.task = $filter('filter')(tasks, {task_id: steroids.view.params['id']})[0];
   });
